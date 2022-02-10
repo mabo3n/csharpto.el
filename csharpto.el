896b9ab4a2909b1bd13ecf1bc3c5d6fe49b9872d
Marcel <dolly.marcel@gmail.com>
2022-02-10 02:06:16 -0300

    Add some functions to facilitate binding the text objects

--- csharpto.el
+++ csharpto.el
@@ -44,6 +44,59 @@
   :type 'line
   (csharpto--get-function-range t))
 
+;;; Bindings
+
+(defvar csharpto-default-bindings-alist
+  '(("if" . csharpto-inner-function)
+    ("af" . csharpto-a-function))
+  "Default bindings for the text objects.")
+
+(defun csharpto-bind-keys (&optional keymaps)
+  "Set default text object bindings.
+
+Bind according to variable `csharpto-default-bindings-alist'
+in each keymap of KEYMAPS. If KEYMAPS is nil, use evil's global
+keymaps (`evil-operator-state-map' and `evil-visual-state-map')."
+  (interactive)
+  (let ((keymaps (or keymaps (list evil-operator-state-map
+                                   evil-visual-state-map))))
+    (dolist (kmap keymaps)
+      (unless (keymapp kmap)
+        (if (keymapp (symbol-value kmap))
+            (setq kmap (symbol-value kmap))
+          (error "Not a keymap: %s" kmap)))
+      (dolist (binding csharpto-default-bindings-alist)
+        (define-key kmap (car binding) (cdr binding))))))
+
+(defun csharpto-bind-keys-locally ()
+  "Set default text object bindings locally.
+
+Call function `csharpto-bind-keys' with evil's local keymaps
+\(`evil-operator-state-local-map' and `evil-visual-state-local-map'\)."
+  (interactive)
+  (csharpto-bind-keys (list evil-operator-state-local-map
+                            evil-visual-state-local-map)))
+
+(defun csharpto--set-default-bindings-for-mode (mode &optional disable)
+  "Make default text object bindings available for MODE.
+
+If DISABLE is non-nil, disable this behavior instead."
+  (let ((mode-hook-sym
+         (intern (concat (symbol-name mode) "-hook"))))
+    (unless (boundp mode-hook-sym)
+      (error "Mode hook not bound: %s" mode-hook-sym))
+    (funcall (if disable #'remove-hook #'add-hook)
+             mode-hook-sym
+             #'csharpto-bind-keys-locally)))
+
+(defun csharpto-use-default-bindings-in-csharp-mode (&optional disable)
+  "Make default text object bindings available for `csharp-mode' only.
+
+When called interactively with `\\[universal-argument]' prefix,
+DISABLE is set to t, which disables the behavior instead."
+  (interactive "P")
+  (csharpto--set-default-bindings-for-mode 'csharp-mode disable))
+
 (provide 'csharpto)
 
 ;;; csharpto.el ends here
