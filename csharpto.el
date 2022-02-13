e1567345761a385a582e628e2e9344af6f60571f
Marcel <dolly.marcel@gmail.com>
2022-02-12 23:05:59 -0300

    Rename `csharpto-bind-keys` to `csharpto-bind-keys-globally`
    And use a "private" function instead. To avoid setting globally accidentaly.
    
    README should be updated.

--- csharpto.el
+++ csharpto.el
@@ -67,31 +67,34 @@
     ("aS" . csharpto-a-SCOPE))
   "Default bindings for the text objects.")
 
-(defun csharpto-bind-keys (&optional keymaps)
-  "Set default text object bindings.
+(defun csharpto--bind-keys (keymaps)
+  "Set default text object bindings in each keymap of KEYMAPS.
 
-Bind according to variable `csharpto-default-bindings-alist'
-in each keymap of KEYMAPS. If KEYMAPS is nil, use evil's global
-keymaps (`evil-operator-state-map' and `evil-visual-state-map')."
+Bind according to variable `csharpto-default-bindings-alist'."
   (interactive)
-  (let ((keymaps (or keymaps (list evil-operator-state-map
-                                   evil-visual-state-map))))
-    (dolist (kmap keymaps)
-      (unless (keymapp kmap)
-        (if (keymapp (symbol-value kmap))
-            (setq kmap (symbol-value kmap))
-          (error "Not a keymap: %s" kmap)))
-      (dolist (binding csharpto-default-bindings-alist)
-        (define-key kmap (car binding) (cdr binding))))))
+  (dolist (kmap keymaps)
+    (unless (keymapp kmap)
+      (if (keymapp (symbol-value kmap))
+          (setq kmap (symbol-value kmap))
+        (error "Not a keymap: %s" kmap)))
+    (dolist (binding csharpto-default-bindings-alist)
+      (define-key kmap (car binding) (cdr binding)))))
+
+(defun csharpto-bind-keys-globally ()
+  "Set default text object bindings in Evil's global keymaps.
+
+\(`evil-operator-state-map' and `evil-visual-state-map'\)."
+  (interactive)
+  (csharpto--bind-keys (list evil-operator-state-map
+                             evil-visual-state-map)))
 
 (defun csharpto-bind-keys-locally ()
-  "Set default text object bindings locally.
+  "Set default text object bindings in Evil's local keymaps.
 
-Call function `csharpto-bind-keys' with evil's local keymaps
 \(`evil-operator-state-local-map' and `evil-visual-state-local-map'\)."
   (interactive)
-  (csharpto-bind-keys (list evil-operator-state-local-map
-                            evil-visual-state-local-map)))
+  (csharpto--bind-keys (list evil-operator-state-local-map
+                             evil-visual-state-local-map)))
 
 (defun csharpto--set-default-bindings-for-mode (mode &optional disable)
   "Make default text object bindings available for MODE.
